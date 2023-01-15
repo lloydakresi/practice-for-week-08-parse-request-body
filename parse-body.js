e214c7cfbb5128be75b21433b074c9f18f6626f4
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-15 18:16:20 +0000

    complete

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,56 @@
 function firstStep(input) {
   // Your code here
+  return input.split("&");
 }
 
 function secondStep(input) {
   // Your code here
+  let arr = input.map(pair =>{
+    return pair.split("=");
+  })
+
+  return arr;
 }
 
 function thirdStep(input) {
   // Your code here
+  let arr = input.map(pair =>{
+    return pair.map(ele =>{
+      ele = ele.replace(/\+/g, " ");
+      return ele;
+    })
+  });
+
+  return arr;
+
+
 }
 
 function fourthStep(input) {
   // Your code here
+  let arr = input.map(pair =>{
+    return pair.map(ele =>{
+      ele = decodeURIComponent(ele)
+      return ele;
+    })
+  })
+
+  return arr;
 }
 
 function fifthStep(input) {
   // Your code here
+  let object = {};
+  input.forEach(pair =>{
+    object[pair[0]] = pair[1];
+  })
+
+  return object;
 }
 
 function parseBody(str) {
   // Your code here
+  return fifthStep(fourthStep(thirdStep(secondStep(firstStep(str)))))
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -6,6 +6,30 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require("http");
+let port = 5000;
+server = http.createServer((req, res)=>{
+    console.log(req.method);
+    console.log(req.url);
+
+    let reqBody = "";
+    req.on("data", (data)=>{
+        reqBody+= data;
+    })
+
+    req.on("end", () =>{
+        req.body = parseBody(reqBody);
+        sendFormPage(req, res);
+    })
+
+
+
+
+
+});
+server.listen(port, ()=>{
+    console.log(`Successfully started the server on port ${port}`);
+})
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
